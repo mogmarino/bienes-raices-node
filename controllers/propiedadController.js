9040c7204f00280690a26e683dfbc8c931f9c8d3
mogmarino <mogmarino@gmail.com>
2023-04-01 18:00:50 -0300

    add form crear propiedad

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -5,4 +5,11 @@ const admin = (req, res) => {
   });
 };
 
-export { admin };
+const crear = (req, res) => {
+  res.render("propiedades/crear", {
+    pagina: "Crear Propiedad",
+    barra: true,
+  });
+};
+
+export { admin, crear };
