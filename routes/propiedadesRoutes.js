4cc74dac8893aa02107f884691189bb305d0c656
mogmarino <mogmarino@hotmail.com>
2023-04-11 22:14:24 -0300

    add validacion agregarImagen

--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -34,6 +34,6 @@ router.post(
 
   guardar
 );
-router.get("/propiedades/agregar-imagen/:id", agregarImagen);
+router.get("/propiedades/agregar-imagen/:id", protegerRuta, agregarImagen);
 
 export default router;
